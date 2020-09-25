9a71595cb3de477ff5f47f9442d134a1d5b6449f
dsijakovski98 <dsijakovski98@github.com>
2020-09-25 14:19:43 +0200

    Updates

---
+++ precache-manifest.8de6bbfa9318316bfe3976773b841e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4b88acf393a65167d152ce7118d877",
+    "url": "/melo-d-app/index.html"
+  },
+  {
+    "revision": "8fd4f2f7e0ad5ce45e80",
+    "url": "/melo-d-app/static/css/main.50166c3e.chunk.css"
+  },
+  {
+    "revision": "858b1bdb1eac9923b85d",
+    "url": "/melo-d-app/static/js/2.734b6da0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/melo-d-app/static/js/2.734b6da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd4f2f7e0ad5ce45e80",
+    "url": "/melo-d-app/static/js/main.c02149dd.chunk.js"
+  },
+  {
+    "revision": "7076a7d29c49a006a5f3",
+    "url": "/melo-d-app/static/js/runtime-main.a97d7603.js"
+  }
+]);
